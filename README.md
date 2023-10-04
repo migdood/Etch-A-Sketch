1409bd471438dd2b8fb1d70a08a27e73c6ca93f1
migdood <almigdadbolad@gmail.com>
2023-10-04 14:53:30 +0300

    Added footer, added favicon, fixed flex calc now all divs fit in the square

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Etch-A-Sketch
 For the Odin Project.
+
+---
+## log if additions
+
+- added footer
+- fixed the flex calc issues so now everything will fit into square div
+- cleaned JS
+- working on rainbow button
+- working on eraser button
+- working on togglable button
--- script/script.js
+++ script/script.js
@@ -1,43 +1,41 @@
 const gridSizeSlider = document.getElementById("gridSize");
 const squareDiv = document.querySelector(".square-div");
-const gridSizeNumber = document.getElementById("gridSizeNumber");
+const gridNumber = document.getElementById("gridNumber");
+
+const blackButton = document.getElementById("blackButton");
+const rainbowButton = document.getElementById("rainbowButton");
+const whiteButton = document.getElementById("whiteButton");
 const clearButton = document.getElementById("clearButton");
 let globalGridSize = 16;
+let colorMode = black;
 
 gridSizeSlider.addEventListener("input", function () {
   const gridSize = gridSizeSlider.value;
   globalGridSize = gridSize;
-  gridSizeNumber.textContent = `${gridSize} x ${gridSize}`;
-  // Update your grid size here, e.g., by creating a new grid with the desired number of divs.
-  // You may need to remove the existing divs and generate a new set of divs.
-  updateGridSize(gridSize);
+  gridNumber.textContent = `${gridSize} x ${gridSize}`;
+  const flexvalue = `calc(100% / ${gridSize})`;
+
+  squareDiv.innerHTML = "";
+
+  createGrid(gridSize, flexvalue);
 });
 
-function updateGridSize(size) {
-  // You can implement your logic to update the grid size here
-  // For example, remove existing divs and generate a new set of divs
-  squareDiv.innerHTML = ""; // Clear existing divs
-  createGrid(size);
-}
+function createGrid(size, flexBasis) {
+  for (let j = 0; j < size * size; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+    cell.style.flexBasis = flexBasis;
+    squareDiv.appendChild(cell);
 
-function createGrid(size) {
-  // Generate a new grid with 'size' number of divs
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      const cell = document.createElement("div");
-      cell.classList.add("grid-cell");
-      squareDiv.appendChild(cell);
-
-      cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "black";
-      });
-    }
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = "black";
+    });
   }
 }
 
-// Initial grid creation with default size (16)
-createGrid(16);
+createGrid(globalGridSize, `calc(100% / ${globalGridSize})`);
 
 clearButton.addEventListener("click", () => {
-  updateGridSize(globalGridSize);
+  squareDiv.innerHTML = "";
+  createGrid(globalGridSize, `calc(100% / ${globalGridSize})`);
 });
