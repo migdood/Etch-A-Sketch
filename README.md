cfe1157d717bcc3bc6372fcea1b884b8861f04f1
migdood <almigdadbolad@gmail.com>
2023-10-05 17:10:25 +0300

    Added rainbow brush, added user docs, fixed the active buttons

--- README.md
+++ README.md
@@ -1,12 +1,88 @@
-# Etch-A-Sketch
-For the Odin Project.
+# Etch-A-Sketch User Guide
+
+Welcome to Etch-A-Sketch, a fun and creative web application where you can draw, sketch, and express your artistic ideas. This guide will help you get started with the features and functions of the Etch-A-Sketch application.
+
+## Table of Contents
+
+1. **Getting Started**
+   - Accessing Etch-A-Sketch
+   - The Main Interface
+
+2. **Drawing**
+   - Selecting a Drawing Mode
+   - Painting on the Canvas
+   - Rainbow Mode
+
+3. **Changing the Grid**
+   - Adjusting the Grid Size
+   - Clearing the Canvas
+
+4. **Additional Information**
+   - Feedback and Support
 
 ---
-## log if additions
 
-- added footer
-- fixed the flex calc issues so now everything will fit into square div
-- cleaned JS
-- working on rainbow button
-- working on eraser button
-- working on togglable button
+## 1. Getting Started
+
+### Accessing Etch-A-Sketch
+
+To start using Etch-A-Sketch, open your web browser and navigate to the URL where the application is hosted.
+
+### The Main Interface
+
+Etch-A-Sketch provides a simple and intuitive interface:
+
+- **Title**: At the top of the page, you will see the title "Etch-A-Sketch."
+
+- **Drawing Area**: In the main section of the page, you'll find a square drawing area with buttons to the left.
+
+- **Buttons**: The buttons on the left allow you to select different drawing modes and interact with the canvas.
+
+## 2. Drawing
+
+### Selecting a Drawing Mode
+
+Etch-A-Sketch offers three drawing modes:
+
+- **Solid**: Allows you to draw with a solid black color.
+- **Rainbow**: Lets you create colorful drawings with random colors.
+- **Eraser**: Erases any existing drawings by setting the background to white.
+
+To select a drawing mode, click on one of the buttons labeled "Solid," "Rainbow," or "Eraser."
+
+### Painting on the Canvas
+
+Once you've chosen a drawing mode, move your mouse cursor over the canvas area. As you move your cursor, you can start drawing by clicking and holding the left mouse button. Move the cursor across the canvas to create your artwork.
+
+### Rainbow Mode
+
+In Rainbow mode, the application automatically changes the color with each brushstroke. Enjoy creating vibrant and dynamic drawings with a variety of colors.
+
+## 3. Changing the Grid
+
+### Adjusting the Grid Size
+
+Etch-A-Sketch allows you to change the size of the drawing grid. To adjust the grid size:
+
+1. Locate the "Grid Size" slider below the canvas.
+2. Click and drag the slider handle to the left or right to increase or decrease the grid size.
+3. Release the slider handle to apply the new grid size.
+
+The grid size represents the number of cells in both horizontal and vertical directions, such as "16x16" or "32x32."
+
+### Clearing the Canvas
+
+To clear the canvas and start fresh:
+
+1. Click the "Clear" button on the left side of the page.
+2. The canvas will be cleared, and the grid size will be reset to the previous setting.
+
+## 4. Additional Information
+
+### Feedback and Support
+
+If you have any questions, feedback, or encounter issues while using Etch-A-Sketch, please feel free to reach out to us. You can find the creator of this application on GitHub for support and updates.
+
+- Creator: [migdood](https://github.com/migdood)
+
+Thank you for using Etch-A-Sketch! Have fun creating your digital artwork and sketches.
--- script/script.js
+++ script/script.js
@@ -1,41 +1,98 @@
 const gridSizeSlider = document.getElementById("gridSize");
 const squareDiv = document.querySelector(".square-div");
-const gridNumber = document.getElementById("gridNumber");
+const gridNumber = document.getElementById("gridSizeNumber");
 
 const blackButton = document.getElementById("blackButton");
 const rainbowButton = document.getElementById("rainbowButton");
-const whiteButton = document.getElementById("whiteButton");
+const eraserButton = document.getElementById("eraserButton");
 const clearButton = document.getElementById("clearButton");
+
 let globalGridSize = 16;
-let colorMode = black;
+let currentColorMode = "black";
+
+function activeButton(color) {
+  if (color == "black") {
+    eraserButton.classList.remove("active");
+    eraserButton.classList.add("default");
+    rainbowButton.classList.remove("active");
+    rainbowButton.classList.add("default");
+    blackButton.classList.add("active");
+    blackButton.classList.remove("default");
+  } else if (color == "rainbow") {
+    eraserButton.classList.remove("active");
+    eraserButton.classList.add("default");
+    rainbowButton.classList.add("active");
+    rainbowButton.classList.remove("default");
+    blackButton.classList.remove("active");
+    blackButton.classList.add("default");
+  } else if (color == "eraser") {
+    eraserButton.classList.add("active");
+    eraserButton.classList.remove("default");
+    rainbowButton.classList.remove("active");
+    rainbowButton.classList.add("default");
+    blackButton.classList.remove("active");
+    blackButton.classList.add("default");
+  }
+}
+blackButton.onclick = () => {
+  activeButton("black");
+  currentColorMode = "black";
+};
+rainbowButton.onclick = () => {
+  activeButton("rainbow");
+  currentColorMode = "rainbow";
+};
+eraserButton.onclick = () => {
+  activeButton("white");
+  currentColorMode = "white";
+};
+clearButton.onclick = () => clearEverything();
+
+function clearEverything() {
+  squareDiv.innerHTML = "";
+  gridSizeSlider.value = globalGridSize;
+  let flexBasis = `calc(100% / ${globalGridSize})`;
+  createGrid(globalGridSize, flexBasis);
+}
 
 gridSizeSlider.addEventListener("input", function () {
-  const gridSize = gridSizeSlider.value;
-  globalGridSize = gridSize;
-  gridNumber.textContent = `${gridSize} x ${gridSize}`;
-  const flexvalue = `calc(100% / ${gridSize})`;
+  globalGridSize = gridSizeSlider.value;
+  gridSizeSlider.value = globalGridSize;
+  gridNumber.textContent = `${globalGridSize} x ${globalGridSize}`;
 
+  let flexBasis = `calc(100% / ${globalGridSize})`;
   squareDiv.innerHTML = "";
-
-  createGrid(gridSize, flexvalue);
+  createGrid(globalGridSize, flexBasis);
 });
 
-function createGrid(size, flexBasis) {
+function createGrid(size, flexBasisVar) {
   for (let j = 0; j < size * size; j++) {
     const cell = document.createElement("div");
     cell.classList.add("grid-cell");
-    cell.style.flexBasis = flexBasis;
+    cell.style.backgroundColor = "white";
+    cell.style.flexBasis = flexBasisVar;
     squareDiv.appendChild(cell);
 
-    cell.addEventListener("mouseover", () => {
-      cell.style.backgroundColor = "black";
-    });
+    cell.addEventListener("mouseover", paint);
   }
 }
 
-createGrid(globalGridSize, `calc(100% / ${globalGridSize})`);
+function paint(e) {
+  if (currentColorMode === "rainbow") {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    let rainbowColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    e.target.style.backgroundColor = rainbowColor;
+  } else if (currentColorMode === "black") {
+    e.target.style.backgroundColor = currentColorMode;
+  } else if (currentColorMode === "white") {
+    e.target.style.backgroundColor = currentColorMode;
+  }
+}
 
-clearButton.addEventListener("click", () => {
-  squareDiv.innerHTML = "";
+window.onload = () => {
   createGrid(globalGridSize, `calc(100% / ${globalGridSize})`);
-});
+  gridSizeSlider.value = globalGridSize;
+  activeButton("black");
+};
